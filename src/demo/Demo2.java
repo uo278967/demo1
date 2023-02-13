ffc12fe10af3405ba3161cee5b0d5c9a790b4fac
Pablo <Pablo@DESKTOP-E02Q75U>
2023-02-13 23:51:08 +0100

    después de merge

--- src/demo/Demo2.java
+++ src/demo/Demo2.java
@@ -1,5 +1,10 @@
 package demo;
 
 public class Demo2 {
-
+	public static void main(String[] args) {
+		System.out.println("Cambios para la rama cambio2");
+		
+		System.out.println("Otro cambio para master");
+	}
+	
 }
